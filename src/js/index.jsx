1551eb86ef61f0d10fea072236b3df3a78c857a5
Roman <ryzhakov.r@mail.ru>
2018-02-05 23:43:11 +0300

    to list and keys

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+class Clock extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount () {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timerID);
+    }
+
+    tick () {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render () {
+        return <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>;
+    }
 }
 
-const element = <Welcome name="Sara" />;
-ReactDOM.render(
-    element,
-    document.getElementById('root')
+setInterval(function (){
+    ReactDOM.render(
+        <Clock />,
+        document.getElementById('root')
+    )},
+    1000
 );
